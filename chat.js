15122325038757dce906156cae62e67474f50e7c
AlaaAbuHamdah <133971859+AlaaAbuHamdah@users.noreply.github.com>
2026-04-05 15:53:57 +0300

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "4ceed6d8-0311-4141-82e5-931c3d8b2b19";
+(function () {
+  d = document;
+  s = d.createElement("script");
+  s.src = "https://client.crisp.chat/l.js";
+  s.async = 1;
+  d.getElementsByTagName("head")[0].appendChild(s);
+})();
